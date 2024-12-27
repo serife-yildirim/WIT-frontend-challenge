2c4d128614c2613181628c015772748ac7397a96
serife-yildirim <serifeozyurek-94@hotmail.com>
2024-12-27 12:51:32 +0300

    Header.jsx güncellendi: Yeni Tailwind sınıfları eklendi

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: { 
+      colors: {
+      primary: '#EA2678', // Özel birincil renk
+      secondary: '#525252', // Ek renk
+      background: '#F3F4F6', // Arka plan için özel renk
+    },
+    boxShadow: {
+      custom: '5px 5px 20px #EA2678', // Hover efektlerinde kullanmak için
+    },
+    spacing: {
+      '18': '4.5rem', // İhtiyaç duyduğunuz ekstra spacing'ler
+    },
   },
+},
   plugins: [],
 };
